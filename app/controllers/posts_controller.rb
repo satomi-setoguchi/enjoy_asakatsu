bceb24cdb797debb53b4c09b8be2e2b8ee309630
瀬戸口理美 <setoguchisatomi@setoguchisatominoiMac.local>
2024-09-14 06:26:19 +0900

    投稿詳細機能の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,14 +7,38 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
+  def show
+    @post = Post.find(params[:id])
+  end
+
   def create
     @post = current_user.posts.build(post_params)
     if @post.save
-      redirect_to posts_path, success: t('flash_message.created', item: '投稿')
+      redirect_to posts_path, success: t('flash_message.created', item: Post.model_name.human)
     else
-      flash.now[:danger] = t('flash_message.not_created', item: '投稿')
+      flash.now[:danger] = t('flash_message.not_created', item: Post.model_name.human)
       render :new, status: :unprocessable_entity
     end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = current_user.posts.find(params[:id])
+    if @post.update(post_params)
+      redirect_to post_path(@post), success: t('flash_message.updated', item: Post.model_name.human)
+    else
+      flash.now[:danger] = t('flash_message.not_updated', item: Post.model_name.human)
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    post = current_user.posts.find(params[:id])
+    post.destroy!
+    redirect_to posts_path, status: :see_other, success: t('flash_message.deleted', item: Post.model_name.human)
   end
 
   private
