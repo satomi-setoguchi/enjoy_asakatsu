ad3c2b152040507a1b8695d993972c046753a6bf
瀬戸口理美 <setoguchisatomi@setoguchisatominoiMac.local>
2024-09-15 21:16:34 +0900

    デプロイの準備12

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,9 @@ set -e # エラーが発生するとスクリプトを終了する意味
 # server.pid が存在するとサーバーが起動できない対策のために server.pid を削除するように設定
 rm -f /enjoy_asakatsu/tmp/pids/server.pid
 
+bundle install
+bundle exec rake assets:precompile
+bundle exec rake assets:clean
+bundle exec rake db:migrate
 # DockerfileのCMDで渡されたコマンド（Railsサーバー起動）を実行
 exec "$@"
