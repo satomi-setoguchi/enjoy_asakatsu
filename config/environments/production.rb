dc5557f71c27888da5d76f2a1b83778376c396f6
瀬戸口理美 <setoguchisatomi@setoguchisatominoiMac.local>
2024-09-15 13:19:10 +0900

    デプロイの準備4

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,6 @@ Rails.application.configure do
 
   # Disable serving static files from `public/`, relying on NGINX/Apache to do so instead.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present? || ENV['RENDER'].present?
-end
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
